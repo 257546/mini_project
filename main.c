c5e0ee7f01c2fb254846c6aeaf844b5850e7f30e
257546 <80680058+257546@users.noreply.github.com>
2021-04-10 00:20:58 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: ARCHANA RAVINDRA
+ *
+ * Created on 9 April, 2021, 9:09 PM
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <ctype.h>
+#include "main.h"
+
+int main(void)
+{
+  int player, comp, result;
+
+  srand((unsigned)time(NULL));
+  for(;;)
+  {
+    player = get_player_choice();
+    if (player == QUIT)
+      return 0;
+    comp = get_computer_choice();
+    result = get_result(player, comp);
+    show_result(player, comp, result);
+  }
+  return 0;
+}
+
+int get_player_choice()
+{
+  char buffer[8];
+  char *pos;
+
+  for(;;)
+  {
+    printf("\nPlease choose rock, paper, scissor or quit: ");
+    fgets(buffer,sizeof(buffer), stdin); 
+    
+    pos = strrchr(buffer, '\n');
+    if (pos != NULL)
+      *pos = '\0'; // remove trailing \n
+
+    if (strcmp(buffer, "rock") == EQUAL)
+      return ROCK;
+    else if (strcmp(buffer, "scissor") == EQUAL)
+      return SCISSOR;
+    else if (strcmp(buffer, "paper") == EQUAL)
+      return PAPER;
+    else if (strcmp(buffer, "quit") == EQUAL)
+      return QUIT;
+  }
+  return 0;
+}
+int get_computer_choice()
+{
+  return rand() % 3;
+}
+
+int get_result(int player, int computer)
+{
+  int idx;
+
+  REQUIRE(player >= 0 && player <= 2);
+  REQUIRE(computer >= 0 && computer <= 2);
+
+  for (idx = 0; idx < NUM_RESULTS; ++idx)
+  {
+    if (results[idx].player == player && results[idx].computer == computer)
+      break;
+  }
+  ENSURE(idx >= 0 && idx < NUM_RESULTS);
+
+  return results[idx].result;
+}
+void show_result(int player, int comp, int result)
+{
+  const char *result_names[] = {"Computer won", "Player won", "Draw"}; 
+  const char *choises[] = {"Paper", "Rock", "Scissor"};
+
+  REQUIRE(result >= COMPUTER_WON && result <= DRAW);
+  REQUIRE(player >= PAPER && player <= SCISSOR);
+  REQUIRE(comp >= PAPER && comp <= SCISSOR);
+
+  printf("\nYou chose: %s", choises[player]);
+  printf("\nComputer choose: %s", choises[comp]);
+  printf("\nResult: %s", result_names[result - 4]);
+}
+
